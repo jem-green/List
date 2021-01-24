8a5a19d1f080b0fed37fa2d6ef0e690387b3266f
Jeremy Green <jem.green@outlook.com>
2021-01-24 13:57:52 +0000

    Fix namespace

--- Program.cs
+++ Program.cs
@@ -42,11 +42,10 @@ namespace List
             Console.WriteLine("data[1]=" + l[1]);
             //Console.WriteLine("data[2]=" + l[2]);
 
-            foreach (string s in q)
+            foreach (string s in l)
             {
                 Console.WriteLine("Enumerate " + s);
             }
-
 
             Console.WriteLine("--------");
 
@@ -80,6 +79,11 @@ namespace List
             Console.WriteLine("data[0]=" + pl[0]);
             Console.WriteLine("data[1]=" + pl[1]);
             //Console.WriteLine("data[2]=" + pl[2]);
+
+            //foreach (string s in pl)
+            //{
+            //    Console.WriteLine("Enumerate " + s);
+            //}
 
         }
     }
